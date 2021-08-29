75cb60aab61dc9fb540ba9375e7548833fbb1b04
nhatlebka <84030512+nhatlebka@users.noreply.github.com>
2021-08-29 11:08:31 +0700

    aaaaa

--- src/routers/index.js
+++ src/routers/index.js
@@ -1,7 +1,10 @@
 const sitesRouter = require('./sites');
 
 const HairRouter = require('./hair');
+const hairProblems = require('./hairProblems');
+
 function routers(app) {
+    app.use('/hair-problems', hairProblems);
     app.use('/hair', HairRouter);
     app.use('/', sitesRouter);
 }
