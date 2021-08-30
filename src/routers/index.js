2a9f3ec3ffb5aef97ca2b0396afe79db803d3ca1
Đặng Minh Hải <637817@sv.vnua.edu.vn>
2021-08-30 12:50:47 +0700

    haidm-update service folder

--- src/routers/index.js
+++ src/routers/index.js
@@ -2,8 +2,10 @@ const sitesRouter = require('./sites');
 
 const HairRouter = require('./hair');
 const hairProblems = require('./hairProblems');
+const hairService = require('./hairService');
 
 function routers(app) {
+    app.use('/services', hairService);
     app.use('/hair-problems', hairProblems);
     app.use('/hair', HairRouter);
     app.use('/', sitesRouter);
